dd22f2a3dcc60b74880a89a3c0ca9333245800bb
Prathmeshdeshmukh <87267186+Prathmeshdeshmukh@users.noreply.github.com>
2023-02-24 19:49:55 +0530

    Added user Auth mmiddleware

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const UserController = require('../../controllers/user-controller');
+const {authReqValidate} = require('../../middlewares/index');
 
-const router = express.Router();
+const router =  express.Router();
 
 router.post('/signup' , UserController.create );
-router.post('/signin' , UserController.signIn );
+router.post('/signin' ,
+ authReqValidate.validateUserAuth
+ ,  UserController.signIn );
 
 router.get('/user/:id' , UserController.get);
 
